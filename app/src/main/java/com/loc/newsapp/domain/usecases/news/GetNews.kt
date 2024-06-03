210e6756507669333aac5694ac19297abb73475e
xidl <26184016+xidsyed@users.noreply.github.com>
2024-06-03 16:45:16 +0530

    Added NewsUseCases to GetNews

---
+++ app/src/main/java/com/loc/newsapp/domain/usecases/news/GetNews.kt
@@ -0,0 +1,13 @@
+package com.loc.newsapp.domain.usecases.news
+
+import androidx.paging.PagingData
+import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetNews(private val newsRepository: NewsRepository) {
+    operator fun invoke(sources: List<String>): Flow<PagingData<Article>> {
+        return newsRepository.getNews(sources = sources)
+    }
+}
